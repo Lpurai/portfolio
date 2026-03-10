3fa37c8e549eb504de407dbdfc157faf65c0ee6e
lpuraiProjects <lpurailekalantula@example.com>
2026-03-10 21:52:34 +0300

    prepare for deployment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,15 +11,16 @@ const Home = () => {
   return (
     <>
  <div className='p-0 mt-16 px-0'>  
-     
-     <div className="flex flex-col mt-2 sm:flex-row">
+     <h2 className="pt-5 md:hidden">Portfolio</h2>
+     <div className="flex flex-col  sm:flex-row">
        <div className=" flex flex-row space-x-5 order-3 w-full h-1/4 sm:flex-col sm:order-1 p-4 justify-center sm:space-y-5 mb-25 sm:h-screen sm:w-1/4">
          <div><a href="https://linkedin.com/in/LpuraiLekalantula" target='_blank'><img src={linkedn} alt="linkedn-icon" className='img-icon'/></a> </div>
                    <div><a href="https://github.com/lpurai" target='_blank'><img src={github} alt="github-icon" className='img-icon' /> </a></div>
                    <div><a href="https://instagram.com/codemaestro_25" target='_blank'><img src={instagram} alt="instagram-icon" className='img-icon' /></a></div>
                    <div><a href="https://x.com/lpurai6624" target='_blank'><img src={twitter} alt="twitter-icon" className='img-icon'/></a></div>
        </div>
-       <div className=" order-1 flex flex-col space-y-5 mt-22 h-1/2 p-5 w-full sm:order-2 sm:w-1/2 sm:h-screen">
+       <div className=" order-1 flex flex-col space-y-5 mt-10 h-1/2 p-5 w-full sm:order-2 sm:w-1/2 sm:h-screen">
+         
          <p> &#128075;Hey</p>
          <h1 className="font-extrabold text-4xl font-serif">I'm Lpurai Lekalantula</h1>
          <p>
