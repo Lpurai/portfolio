602c2bfb423d8f26f2121f3d38ac1d55fe9a22c1
lpuraiProjects <lpurailekalantula@example.com>
2026-03-10 14:14:42 +0300

    change repo name

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
-  <BrowserRouter basename='/my-portfolio'>
+  <BrowserRouter basename='/portfolio'>
     <App />
   </BrowserRouter>
  
